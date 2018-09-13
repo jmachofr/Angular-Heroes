e4be01a02bed91a4b8ea537b8efca08b1a5ce297
Julian Macho Fraga <machofraga.julian@hotmail.com>
2018-09-13 16:52:36 -0300

    qwe

---
+++ src/app/mock-heroes.ts
@@ -0,0 +1,14 @@
+import { Hero } from './hero';
+
+export const HEROES: Hero[] = [
+{id: 11, name: 'hero11'},
+{id: 12, name: 'hero12'},
+{id: 13, name: 'hero13'},
+{id: 14, name: 'hero14'},
+{id: 15, name: 'hero15'},
+{id: 16, name: 'hero16'},
+{id: 17, name: 'hero17'},
+{id: 18, name: 'hero18'},
+{id: 19, name: 'hero19'},
+{id: 20, name: 'hero20'},
+];
